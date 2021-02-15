2fac4d094c6c003761b3522908c88e61d3e11f99
Leftyxiv <manny@aledoux.net>
2021-02-15 15:19:33 -0500

    commited to push

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React from "react";
+import LanguageContext from "../context/LanguageContext";
+import ColorContext from "../context/ColorContext";
+
+class Button extends React.Component {
+  // static contextType = LanguageContext;
+
+  render() {
+    //  const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return (
+      <ColorContext.Consumer>
+        {(color) => 
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {({language}) => (language === "english" ? "Submit" : "Voorleggen")}
+            </LanguageContext.Consumer>
+          </button>
+        }
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
